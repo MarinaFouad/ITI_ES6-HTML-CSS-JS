145e572c9f51642644fd4d3215a3d38415bc8403
MarinaFouad <108586666+MarinaFouad@users.noreply.github.com>
2024-01-19 19:37:43 +0200

    Add files via upload

---
+++ FrontEND/Javascript/lab1/lab.js
@@ -0,0 +1,14 @@
+/*
+var fName=prompt("Enter your first name");
+var sName=prompt("Enter your second name");
+var r = confirm("confirm your name is " + fName +" " + sName);
+var year=prompt("Enter your birth year");
+let age = 2023-year;
+alert("welcome to this page " +fName +" " +sName+" your age is " +age);
+*/
+
+alert("first release of the calculator");
+var fNum=prompt("Enter first number");
+var sNum=prompt("Enter second number");
+var sum=Number(fNum) +Number(sNum);
+alert("the sum is "+sum );
